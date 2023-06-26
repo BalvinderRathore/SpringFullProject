23603e91b2aa487bf3d100b119c234dc736504c4
Balvinder Singh <rathorebalvinder.007@gmail.com>
2023-06-26 15:48:36 +0530

    Second Commit

---
+++ src/main/java/com/example/springfullproject/MainRestController.java
@@ -0,0 +1,30 @@
+package com.example.springfullproject;
+
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MainRestController {
+
+
+    @Autowired
+    UserdetailRepository userdetailRepository;
+
+
+    @PostMapping("getuserbyemail")
+    public ResponseEntity<Userdetail> getUserdetailsByEmail(@RequestParam("email") String email){
+        if (userdetailRepository.findByEmail(email).isPresent()){
+            return new ResponseEntity<>(userdetailRepository.findByEmail(email).get(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+
+}
--- src/main/java/com/example/springfullproject/UsertypelinkRepository.java
+++ src/main/java/com/example/springfullproject/UsertypelinkRepository.java
@@ -1,6 +1,10 @@
 package com.example.springfullproject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UsertypelinkRepository extends JpaRepository<Usertypelink, String> {
+
 }
